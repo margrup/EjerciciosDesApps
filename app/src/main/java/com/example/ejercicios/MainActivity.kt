e3fb634d00bdcb8aa344deafc5d3ce28b3d2237b
Martín Grupillo <mgrupillo@uade.edu.ar>
2026-08-28 20:29:00 -0300

    Ejercicio 2 completado.

--- app/src/main/java/com/example/ejercicios/MainActivity.kt
+++ app/src/main/java/com/example/ejercicios/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EjerciciosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Presentacion(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +31,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Presentacion(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Nombre: [Tu Nombre]")
+        Text(text = "Carrera: [Tu Carrera]")
+        Text(text = "Tecnología a aprender: Jetpack Compose")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PresentacionPreview() {
     EjerciciosTheme {
-        Greeting("Android")
+        Presentacion()
     }
 }
